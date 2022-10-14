0a3bcd34e7dd77edc5cda93d361a13b7af3487da
Xelem <anselmchizurum@gmail.com>
2022-10-14 13:23:24 +0100

    Update wallet balance after funding

--- controllers/paystackController.js
+++ controllers/paystackController.js
@@ -1,4 +1,6 @@
 const paystack = require("paystack-api")(process.env.PAYSTACK_SECRET_KEY_DEV);
+const knexConfig = require("../db/knexfile");
+const knex = require("knex")(knexConfig[process.env.NODE_ENV]);
 
 exports.initializeTransaction = async (req, res) => {
   const session = await paystack.transaction.initialize({
@@ -24,10 +26,36 @@ exports.verifyPayment = async (req, res) => {
     });
   }
 
+  //   Update wallet balance
+  const wallet = await knex("wallets")
+    .select({
+      id: "id",
+      user_id: "user_id",
+      wallet_address: "wallet_address",
+      amount: "amount",
+    })
+    .where({ user_id: req.user.id });
+
+  await knex("wallets")
+    .where({ user_id: req.user.id })
+    .update({
+      amount: wallet[0].amount + status.data.amount / 100,
+    });
+
+  const updWallet = await knex("wallets")
+    .select({
+      id: "id",
+      user_id: "user_id",
+      wallet_address: "wallet_address",
+      amount: "amount",
+    })
+    .where({ user_id: req.user.id });
+
   res.status(200).json({
     status: "success",
     message: `Congratulations! You have successfully credited your Lendsqr wallet with ₦${
       status.data.amount / 100
     }`,
+    balance: `₦${updWallet[0].amount}`,
   });
 };
