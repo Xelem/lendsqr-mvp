be60d06318e18267081a7d59e96cdf93b8c0166f
Xelem <anselmchizurum@gmail.com>
2022-10-14 11:55:33 +0100

    Create a function to protect cretain routes

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userController = require("../controllers/userController");
 const walletController = require("../controllers/walletController");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
@@ -10,5 +11,13 @@ router.post(
   walletController.createWallet
 );
 router.post("/login", userController.login);
+
+router.use(authController.protect);
+
+router.get("/test", (req, res) => {
+  res.status(200).json({
+    message: "success",
+  });
+});
 
 module.exports = router;
